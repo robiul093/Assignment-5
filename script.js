938100e46a8638fdf3df92e25b41c7223ad5661e
Robiul@Hasan <robihasan093@gmail.com>
2024-02-17 02:41:43 -0800

    creat js function

---
+++ script.js
@@ -0,0 +1,8 @@
+// const nextBtn = document.getElementById('next-btn');
+// nextBtn.addEventListener('click', function(){
+// });
+
+function nextSection(){
+    const success = document.getElementById('success');
+    success.classList.remove('hidden');
+}
